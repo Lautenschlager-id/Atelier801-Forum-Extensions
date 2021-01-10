1f06cf6620c25a96f15c1ba1a11172f63c61d472
Lautenschlager <tai.romani@hotmail.com>
2021-01-10 01:10:20 -0300

    undo changes

--- BetaColorPicker.user.js
+++ BetaColorPicker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        [A801] Beta Color Picker
 // @namespace   @Bolodefchoco
-// @version     0.3
+// @version     0.2
 // @description Enables a color picker for bbcode. [click in the button after picking the color]
 // @author      @Bolodefchoco
 // @include     https://atelier801.com/*
@@ -13,33 +13,34 @@
 	"use restrict";
 
 	let colorTables = document.getElementsByClassName("dropdown-menu pull-right label-message");
-	let tableIndex = colorTables.length-1
-	let table = colorTables[tableIndex];
+	for (let tableIndex = 0; tableIndex < colorTables.length; tableIndex++) {
+		let table = colorTables[tableIndex];
 
-	let refElement = table.parentElement.firstElementChild.attributes.onclick.value.match('\'(.+?)\'')[1];
+		let refElement = table.parentElement.firstElementChild.attributes.onclick.value.match('\'(.+?)\'')[1];
 
-	table.parentElement.innerHTML += `<button type="button" id="colorpicker_${tableIndex}" class="btn btn-reduit" >
-<img src="/img/sections/pinceau.png" class="img16 espace-2-2">
+		table.parentElement.innerHTML += `<button type="button" id="colorpicker_${tableIndex}" class="btn btn-reduit" >
+	<img src="/img/sections/pinceau.png" class="img16 espace-2-2">
 </button>
 `;
-	let colorPicker = document.getElementById("colorpicker_" + tableIndex);
+		let colorPicker = document.getElementById("colorpicker_" + tableIndex);
 
-	jsColorPicker("button[id^=\"colorpicker_" + tableIndex + "\"]", {
-		noAlpha: true,
-		customBG: "#FFFFFF",
-		appendTo: document.body.parentElement,
-		size: 2
-	})
+		jsColorPicker("button[id^=\"colorpicker_" + tableIndex + "\"]", {
+			noAlpha: true,
+			customBG: "#FFFFFF",
+			appendTo: document.body.parentElement,
+			size: 2
+		})
 
-	let isPicking = false;
+		let isPicking = false;
 
-	colorPicker.addEventListener("click", function(){
-		if (isPicking)
-		{
-			isPicking = false;
-			ajouterBBCode(refElement, "[color=" + colorPicker.value + "]", "[/color]", 15);
-		}
-		else
-			isPicking = true;
-	});
+		colorPicker.addEventListener("click", function(){
+			if (isPicking)
+			{
+				isPicking = false;
+				ajouterBBCode(refElement, "[color=" + colorPicker.value + "]", "[/color]", 15);
+			}
+			else
+				isPicking = true;
+		});
+	}
 })();
