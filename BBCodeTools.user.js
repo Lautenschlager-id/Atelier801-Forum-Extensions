6a849096b355a1ee5f4fd351203c3ad289a9f973
Lautenschlager <tai.romani@hotmail.com>
2021-02-20 15:20:28 -0300

    Fix initial position for Table Center & Lua

--- BBCodeTools.user.js
+++ BBCodeTools.user.js
@@ -40,7 +40,7 @@ function addColorPicker(table, refElement, tableIndex)
 function addObjectCentralizer(textElement, refElement)
 {
 	textElement.children[2].innerHTML += `<li>
-	<a class="element-menu-outils" onclick="ajouterBBCode('${refElement}', '[table align=center][cel]', '[/cel][/table]', 26);">
+	<a class="element-menu-outils" onclick="ajouterBBCode('${refElement}', '[table align=center][cel]', '[/cel][/table]', 25);">
 		<img src="/img/icones/16/edit-alignment-center.png"> Table Center
 	</a>
 </li>`
@@ -49,7 +49,7 @@ function addObjectCentralizer(textElement, refElement)
 function addLuaCode(table, refElement)
 {
 	table.outerHTML += `<div class="btn-group groupe-boutons-barre-outils">
-	<button type="button" class="btn btn-reduit" onclick="ajouterBBCode('${refElement}', '[code=Lua]', '[/code]', 11);" title="Lua Code">
+	<button type="button" class="btn btn-reduit" onclick="ajouterBBCode('${refElement}', '[code=Lua]', '[/code]', 10);" title="Lua Code">
 		<img src="http://images.atelier801.com/177c092b190.png" class="img16 espace-2-2">
 	</button>
 </div>`;
